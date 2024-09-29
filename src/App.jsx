90ef73419587121d278e707e6564610786f9df73
KushalUpreti <kushalupreti101@gmail.com>
2024-09-29 16:10:04 -0500

    Added login/signup UI and validation logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { Login } from "./components/login/login";
+import { Signup } from "./components/signup/signup";
+
 const App = () => {
   return (
-    <div className=''>App</div>
-  )
-}
+    <main className="body-wrapper">
+      <div className="auth-container">
+        <section className="login-section">
+          <Login />
+        </section>
+        <section className="signup-section">
+          <Signup />
+        </section>
+      </div>
+    </main>
+  );
+};
 
-export default App
+export default App;
