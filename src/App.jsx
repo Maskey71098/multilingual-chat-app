e94f055cf6d9ceb5a4a2ca75df4c1e6239fdb440
GKS567 <kavyagutta0@gmail.com>
2024-10-01 15:19:44 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import Chat from "./component/Chat/chat";
+import List from "./component/List/list";
+import Detail from "./component/Detail/Detail";
 const App = () => {
   return (
-    <div className=''>App</div>
-  )
+    <div className='container'>
+      <List/>
+      <Chat/>
+      <Detail/>
+    </div>
+  );
 }
 
 export default App
