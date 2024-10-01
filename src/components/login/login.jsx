aba4be41a63a9072d7b3a2b81e072b1cc8884dfc
KushalUpreti <kushalupreti101@gmail.com>
2024-10-01 15:44:34 -0500

    Hooked UI to firebase backend and handled error/successful login

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -3,17 +3,38 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import "./login.css";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 export const Login = () => {
   const [validated, setValidated] = useState(false);
+  const auth = getAuth();
+
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(false);
 
   const handleSubmit = (event) => {
     const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+    event.preventDefault();
     setValidated(true);
+
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+      return;
+    }
+    setError(null);
+    setSuccess(null);
+    const formData = new FormData(event.target);
+    const { email, password } = Object.fromEntries(formData.entries());
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        setSuccess(true);
+      })
+      .catch((error) => {
+        setError("Invalid credentials!! Try again");
+      });
   };
 
   return (
@@ -28,7 +49,12 @@ export const Login = () => {
         <Row className="mb-3">
           <Form.Group controlId="validationCustom013">
             <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="test@email.com" required />
+            <Form.Control
+              type="email"
+              name="email"
+              placeholder="test@email.com"
+              required
+            />
             <Form.Control.Feedback type="invalid">
               Please provide a valid email.
             </Form.Control.Feedback>
@@ -37,12 +63,20 @@ export const Login = () => {
         <Row className="mb-3">
           <Form.Group controlId="validationCustom014">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="*****" required />
+            <Form.Control
+              type="password"
+              name="password"
+              placeholder="*****"
+              required
+            />
             <Form.Control.Feedback type="invalid">
               Please provide a valid password.
             </Form.Control.Feedback>
           </Form.Group>
         </Row>
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        {success && <p style={{ color: "green" }}>Signup successful!</p>}
+
         <Button type="submit">Sign In</Button>
       </Form>
     </div>
