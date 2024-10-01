fe0e42c8bb82316c70148307251c0f851112de5a
Abhishek Raj Maskey <58427790+Maskey71098@users.noreply.github.com>
2024-10-01 15:02:44 -0500

    Revert "Amaskey main"

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
-import { Login } from "./components/login/login";
-import { Signup } from "./components/signup/signup";
-
 const App = () => {
   return (
-    <main className="body-wrapper">
-      <div className="auth-container">
-        <section className="login-section">
-          <Login />
-        </section>
-        <section className="signup-section">
-          <Signup />
-        </section>
-      </div>
-    </main>
-  );
-};
+    <div className=''>App</div>
+  )
+}
 
-export default App;
+export default App
