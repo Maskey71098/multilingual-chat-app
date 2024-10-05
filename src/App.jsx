8dee008333f05091e1a981601f1a08d5bed7b1d4
pujamudbhari <pujamudbhari1234@gmail.com>
2024-10-05 12:27:29 -0500

    created zustand store & handled logged in scenario

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,52 @@ import { Signup } from "./components/signup/signup";
 import Chat from "./component/Chat/chat";
 import List from "./component/List/list";
 import Detail from "./component/Detail/Detail";
+import { useUserStore } from "./lib/userStore";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./lib/firebase";
 //import UserList from "./components/userList/userList";
 
 const App = () => {
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
+
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      fetchUserInfo(user?.uid);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [fetchUserInfo]);
+
+  if (isLoading) return <div className="loading">Loading...</div>;
   return (
-    <main className="body-wrapper">
-      <div className="auth-container">
-        <section className="login-section">
-          <Login />
-        </section>
-        <section className="signup-section">
-          <Signup />
-        </section>
-        {/* <section>      
+    <>
+      {currentUser ? (
+        <main className="chat-container">
+          <div className="container">
+            <List />
+            <Chat />
+            <Detail />
+          </div>
+        </main>
+      ) : (
+        <main className="body-wrapper">
+          <div className="auth-container">
+            <section className="login-section">
+              <Login />
+            </section>
+            <section className="signup-section">
+              <Signup />
+            </section>
+            {/* <section>      
           <UserList/>
         </section> */}
-        {/* <div className="container">
-          <List />
-          <Chat />
-          <Detail />
-        </div> */}
-      </div>
-    </main>
+          </div>
+        </main>
+      )}
+    </>
   );
 };
 
