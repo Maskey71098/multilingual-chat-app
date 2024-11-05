930e7968bbf79e1be7a873d0539341cd3a9e8a3a
pujamudbhari <pujamudbhari1234@gmail.com>
2024-11-05 10:23:27 -0600

    added api key from env file

--- src/utils/googleTranslate.js
+++ src/utils/googleTranslate.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "AIzaSyAQeLJGe64GuyR60W10T72q3cSG9yIRV-k";
+const API_KEY = import.meta.env.VITE_TRANSLATION_API_KEY;
 const API_URL = "https://translation.googleapis.com/language/translate/v2";
 
 const translateText = async (text, targetLanguage) => {
